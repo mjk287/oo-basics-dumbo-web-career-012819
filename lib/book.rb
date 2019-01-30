3afd4382a602f5353846ab67123f4a0084a84cac
github username <github email address>
2019-01-30 11:23:46 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(book_name)
+    @title = book_name
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
